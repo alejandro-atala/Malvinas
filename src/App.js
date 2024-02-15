a0964fa747a35a17a7ad2fe542c45e6a80672b22
Alejandro <alejandro.atala@municipiodetandil.edu.ar>
2024-02-15 10:43:46 -0300

    img

--- src/App.js
+++ src/App.js
@@ -35,26 +35,26 @@ function App() {
       
   },
   { 
-    titulo: "Historia 1", 
-    descripcion: "Descripción de la historia 1", 
-    imagen: "https://media.canalnet.tv/2021/04/guerra-las-islas-malvinas.png" ,
+    titulo: "Historia 4", 
+    descripcion: "Descripción de la historia 4", 
+    imagen: "https://s3.amazonaws.com/arc-wordpress-client-uploads/infobae-wp/wp-content/uploads/2017/03/15082659/guerra-malvinas-bando-britanico-6.jpg" ,
     autor: "Pedro Garcia",
     fotoAutor: "https://elnueve-compress.s3-accelerate.amazonaws.com/files/1525263728890el%20testimonio%20de%20un%20veterano%20tomba.jpg",
     fecha: "12/05/2023"
 },
 { 
-    titulo: "Historia 2", 
-    descripcion: "Descripción de la historia 2", 
-    imagen: "https://conlagentenoticias.com/wp-content/uploads/2020/10/malvinas-th-768x432.jpg" ,
+    titulo: "Historia 5", 
+    descripcion: "Descripción de la historia 5", 
+    imagen: "https://img.europapress.es/fotoweb/fotonoticia_20170402084523-999309_1024.jpg" ,
     autor: "Mario Rodriguez",
     fotoAutor: "https://c1.staticflickr.com/4/3650/3407092802_8f6ecb75a8.jpg",
     fecha: "12/05/2023"
 },
 
 { 
-  titulo: "Historia 3", 
-  descripcion: "Descripción de la historia 3", 
-  imagen: "https://www.elclarin.cl/wp-content/uploads/2020/04/guerra-de-malvinas-1982-211903.jpg",
+  titulo: "Historia 6", 
+  descripcion: "Descripción de la historia 6", 
+  imagen: "https://static.misionesonline.news/wp-content/uploads/2023/05/ejercito-argentino.jpg",
   autor: "Juan Clark",
   fotoAutor: "https://media.minutouno.com/adjuntos/150/imagenes/024/106/0024106794.jpg",
   fecha: "12/05/2023"
--- src/Imagen/Imagen.js
+++ src/Imagen/Imagen.js
@@ -4,7 +4,7 @@ function Imagen() {
     return (
         <div className="container-fluid p-0 position-relative">
             <img src="https://eltigredepapel.com/wp-content/uploads/2020/11/Malvinas.jpg" className="img-fluid" alt="Imagen principal" style={{ height: '500px', width: '100%', objectFit: 'cover', marginTop: '0px' }} />
-            <div className="position-absolute start-50 translate-middle text-center text-black" style={{ top: '100px' }}>
+            <div className="position-absolute start-50 translate-middle text-center text-black" style={{ top: '50px' }}>
                 <h1>Historias en primera persona</h1>
                 <h3>Narradas por quienes estuvieron allí</h3>
             </div>
